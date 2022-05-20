6a6e5b51995b97733be050eec7d9642d3212c11b
unknown <chadiegilaugis@gmail.com>
2022-05-20 17:31:40 +0800

    initial

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:jube_cc_semi/views/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ services/remote_service.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'package:jube_cc_semi/models/post.dart';
+
+class RemoteService {
+  Future<List<Post>?> getPosts() async {
+    var client = http.Client();
+
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postFromJson(json);
+    }
+  }
+}
